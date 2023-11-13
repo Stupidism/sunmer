eaa22127b235c50b97b1b3b6839bf9337b694b6f
SunStupic <sun@workstream.is>
2023-11-13 09:19:14 +0800

    fixup! fixup! feat: add steps-proxy

--- steps-proxy/src/main.ts
+++ steps-proxy/src/main.ts
@@ -5,11 +5,12 @@ import { createProxyServer } from 'http-proxy';
 // Create a proxy server with an options object
 const proxy = createProxyServer({
   changeOrigin: true, // Recommended for proxying to HTTPS servers
-  secure: false // If false, will accept invalid SSL certificates
+  secure: false, // If false, will accept invalid SSL certificates
 });
 
 // The target website you want to redirect traffic to
-const targetWebsite = 'https://sunmer.vercel.app/';
+const targetWebsite =
+  process.env.TARGET_WEBSITE ?? 'https://www.baidu.com/';
 
 // Create a standard Node.js HTTP server that will pass all requests to the proxy server
 const server = http.createServer((req, res) => {
@@ -17,7 +18,9 @@ const server = http.createServer((req, res) => {
     console.error('error: ', error);
     // If there's an error while proxying, respond with an error message
     res.writeHead(500, { 'Content-Type': 'text/plain' });
-    res.end('The proxy server encountered an error while processing your request.');
+    res.end(
+      'The proxy server encountered an error while processing your request.'
+    );
   });
 });
 
