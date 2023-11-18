cf8f9789b4a202d859df90d5ed2ab6a20eb7367c
SunStupic <sun@workstream.is>
2023-11-18 20:44:03 +0800

    chore: try to setup payload

--- apps/steps/next.config.js
+++ apps/steps/next.config.js
@@ -2,6 +2,8 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+const path = require("path");
+const { withPayload } = require("@payloadcms/next-payload");
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -19,4 +21,24 @@ const plugins = [
   withNx,
 ];
 
-module.exports = composePlugins(...plugins)(nextConfig);
+module.exports = withPayload(
+  composePlugins(...plugins)(nextConfig),
+  {
+    // The second argument to `withPayload`
+    // allows you to specify paths to your Payload dependencies
+    // and configure the admin route to your Payload CMS.
+
+    // Point to your Payload config (Required)
+    configPath: path.resolve(__dirname, "./payload/payload.config.ts"),
+
+    // Point to custom Payload CSS (optional)
+    cssPath: path.resolve(__dirname, "./css/payload.global.css"),
+
+    // Point to your exported, initialized Payload instance (optional, default shown below`)
+    payloadPath: path.resolve(process.cwd(), "./payload/payloadClient.ts"),
+
+    // Set a custom Payload admin route (optional, default is `/admin`)
+    // NOTE: Read the "Set a custom admin route" section in the payload/next-payload README.
+    adminRoute: "/admin",
+  }
+);
