a73ad81175aa097bea981e67dad9a544fc6eacff
SunStupic <vercel@workstream.is>
2024-10-27 10:52:34 +0800

    chore(payload): initialize

---
+++ apps/steps/next.config.mjs
@@ -0,0 +1,32 @@
+import { composePlugins, withNx } from '@nx/next';
+import path from 'path';
+import { withPayload } from '@payloadcms/next/withPayload';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    reactCompiler: false
+  },
+  // this includes files from the monorepo base two directories up
+  outputFileTracingRoot: path.join(__dirname, '../../'),
+  nx: {
+    // Set this to true if you would like to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: false,
+  },
+};
+
+const plugins = [
+  // Add more Next.js plugins to this list if needed.
+  withNx,
+];
+
+export default composePlugins(...plugins)(withPayload(nextConfig));
